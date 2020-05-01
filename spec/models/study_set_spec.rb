130261abaf09b667e5dcfeea8367371db9a00a30
macanudo@yahoo.com <macanudo@yahoo.com>
2020-05-01 11:04:07 +0900

    Add model specs

--- spec/models/study_set_spec.rb
+++ spec/models/study_set_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe StudySet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+  	it { should belong_to(:user) }
+  	it { should belong_to(:link) }
+  	it { should have_and_belong_to_many(:entries) }	
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+  	it { should have_many(:clicks) }
+  	it { should have_many(:links) }
+  	it { should have_many(:study_sets) }	
+  end
 end
