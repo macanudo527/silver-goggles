5feeea97992b18ec137c0e4c716cf2c4b0568cec
macanudo@yahoo.com <macanudo@yahoo.com>
2020-11-19 08:58:01 +0900

    Add Japanese check to Kyodo Processor

--- db/seeds/source_seeds.rb
+++ db/seeds/source_seeds.rb
@@ -2,6 +2,7 @@ sources = []
 columns = [:title, :url, :descrip, :image]
 
 sources << {title: 'Aera dot', url: 'https://dot.asahi.com/', descrip: 'Aera Weekly Magazine', image: 'aera-dot.gif'}
+sources << {title: 'Kyodo', url: 'https://www.47news.jp/', descrip: 'Kyodo News', image: 'https://nordot-res.cloudinary.com/c_limit,w_300,h_300,f_auto,q_auto:eco/ch/units/39166665832988672/profile_7.png'}
 
 Source.import columns, sources, validate: false
 
